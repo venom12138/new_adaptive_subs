4e6e6f977e5b3fac5d53d5c1f920b8ca3800815a
venom12138 <1390852413@qq.com>
2023-05-13 13:55:05 +0800

    0513 sync

--- gen_offline_data.py
+++ gen_offline_data.py
@@ -19,17 +19,20 @@ from supervised.int.int_hacking import sample_axiom_order, \
 from utils import storage
 from supervised.int.gen_subgoal_data import set_rnd_seed_and_generate_problem
 import joblib
+from tqdm import tqdm
 
-def generate_offline_problems(save_path, n_proofs=1e6, n_workers=20, num_proof_per_chunk=1000,
+def generate_offline_problems(save_path, start_chunk=0, n_proofs=1e6, n_workers=20, num_proof_per_chunk=1000,
                             proof_length=15, seed=None, max_length_mode=True):
     num_chunks = n_proofs // num_proof_per_chunk
     np.random.seed(seed)
     if seed is not None:
         torch.manual_seed(seed)
-    for cur_chunk in range(num_chunks):
+    for cur_chunk in tqdm(range(start_chunk, num_chunks)):
         problems = []
         rng = np.random.default_rng(np.random.randint(low=0, high=2**32, size=1)[0])
+        
         seeds = rng.integers(low=0, high=2**63, size=num_proof_per_chunk)
+        # print(seeds)
         problems.extend(
             Parallel(n_jobs=n_workers, verbose=9)(
                 delayed(set_rnd_seed_and_generate_problem)(
@@ -42,4 +45,4 @@ def generate_offline_problems(save_path, n_proofs=1e6, n_workers=20, num_proof_p
         joblib.dump(problems, filename=f'{save_path}/chunk_{cur_chunk:08d}')
 
 if __name__ == '__main__':
-    generate_offline_problems(save_path='./data', n_proofs=1000, n_workers=20, num_proof_per_chunk=200,)
+    generate_offline_problems(save_path='./data', start_chunk=311, n_proofs=1000000, n_workers=60, num_proof_per_chunk=1000,)
